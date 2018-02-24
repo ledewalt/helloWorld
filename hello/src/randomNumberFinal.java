1d58ef2ec522b590d012b9c68384085eb2fa824d
ledewalt <36778638+ledewalt@users.noreply.github.com>
2018-02-24 18:07:25 -0500

    init from laptop

---
+++ hello/src/randomNumberFinal.java
@@ -0,0 +1,22 @@
+public class randomNumberFinal {
+
+    public static void main (String[] args)
+    {
+        //here is the code i used to test
+
+        /*for (int counter = 1; counter <= 1; counter++)
+        {
+            int randomNumber = (int) (Math.random() * 10);
+            System.out.println(randomNumber);
+        }
+
+        System.out.println();*/
+
+        // same results with less code
+
+        int randomNumber = (int) (Math.random() * 10);
+        System.out.println(randomNumber);
+    }
+
+}
+
